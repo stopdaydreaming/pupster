e9192c81e13045e3998115520b72f478e14b1b86
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-17 09:58:17 -0500

    added search functionality, updated discover

--- src/pages/Discover/Discover.jsx
+++ src/pages/Discover/Discover.jsx
@@ -1,10 +1,94 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+
+// hard code the jsx
+// move the values into variables (state, props)
+// pull in the values dynamically
 
 class Discover extends Component {
+
+    state = {
+        image: "",
+        showAlert: false,
+        count: 0,
+    }
+
+    componentDidMount() {
+        this.getNewDog();
+    }
+
+    getNewDog = () => {
+        // console.log("new dog");
+        axios.get('https://dog.ceo/api/breeds/image/random')
+        .then((response) => {
+            this.setState({
+                image: response.data.message,
+            });
+        });
+    };
+
+    handleDislikeButton = () => {
+        this.setState({ showAlert: false });
+        this.getNewDog();
+    }
+
+    handleLikeButton = () => { 
+        this.setState({ showAlert: false });
+        const myNumber = Math.floor(Math.random() * 5 + 1);
+        if(myNumber === 1){
+            this.setState({ showAlert:true, count: this.state.count + 1 });
+        }
+        this.getNewDog();
+    }
+
     render() {
         return (
-            <div>
-                <h1>This the Discover page</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col d-flex justify-content-center">
+                        <img 
+                            src={this.state.image}
+                            alt="pupper"
+                            // style={{ height:400 }}
+                        />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-3"></div>
+                    <div className="col-sm-3 d-flex justify-content-start">
+                        <button 
+                            className="btn btn-danger"
+                            onClick={this.handleDislikeButton}
+                        >
+                        Dislike
+                        </button>
+                    </div>
+                    <div className="col-sm-3 d-flex justify-content-end">
+                        <button 
+                            className="btn btn-success"
+                            onClick={this.handleLikeButton}
+                        >
+                        Like
+                        </button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <h3 className="text-center">
+                            {this.state.count} dogs have liked you back!
+                        </h3>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <div 
+                            className="alert alert-success"
+                            role="alert"
+                        >
+                            Awesome! That dog liked you too!
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,10 +1,59 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 class Search extends Component {
+
+    state = {
+        searchTerm: "",
+    };
+
+    componentDidMount() {
+        this.displayBreeds();
+    }
+
+    displayBreeds = () => {
+        const dogBreedListURL = `https://dog.ceo/api/breeds/list`;
+        axios.get(dogBreedListURL).then((response) => {
+            const dog = response.data.message;
+        });
+    }
+
+    searchBreed = () => {
+        const searchBreedURL = `https://dog.ceo/api/breed/${this.state.searchTerm}/images`;
+        axios.get(searchBreedURL).then((response) => {
+            const dog = response.data.message;
+        });
+    }
+
+    handleInputChange = event => {
+        console.log("handle input change")
+    }
+
+    handleFormSubmit = event => {
+        console.log("handle form submit")
+    }
+
     render() {
         return (
             <div>
-                <h1>This the Search page</h1>
+                <h1>Dog Breed Search page</h1>
+                <form onClick={this.handleFormSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="search">Search:</label>
+                        <input 
+                            onChange={this.handleInputChange}
+                            value={this.state.searchTerm}
+                            name="search"
+                            type="text"
+                            className="form-control"
+                            id="search"
+                        />
+                        <br />
+                        <button className="btn btn-primary">
+                            Search
+                        </button>
+                    </div>
+                </form>
             </div>
         )
     }
